d58457b841e27461a54ee0a66dd2fd81b22c8ce1
BLoveless <bloveless@talentpath.com>
2020-04-20 14:17:00 -0400

    fundamentals

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        // starting at the beginning
+        System.out.println("Hello there world it's Brian L. Making some Java");
+
+        // declare vars SYNTAX
+
+        // type variable = value
+
+        String name = "Brian";
+        System.out.println(name);
+        int myNumber = 33;
+        System.out.println(myNumber);
+        int yourNumber;
+        yourNumber = 15;
+        System.out.println(yourNumber);
+
+        final int theNum = 42;
+
+        System.out.println(theNum);
+
+        // theNum = 17; // will generate an error: cannot assign a value to a final variable
+
+        System.out.println(theNum);
+
+
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        System.out.println(myFloatNum);
+        System.out.println(myBool);
+        System.out.println(myNum);
+        System.out.println(myText);
+        System.out.println(myLetter);
+
+
+    }
+}
